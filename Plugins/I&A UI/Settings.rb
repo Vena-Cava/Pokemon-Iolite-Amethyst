c22753c1881bc7faa9b1724ed83ea76a7f82ec45
Vena Cava <61953449+Vena-Cava@users.noreply.github.com>
2024-12-11 18:15:35 +0000

    Some Updates + Removed Art & Screenshot Files

--- Plugins/I&A UI/Settings.rb
+++ Plugins/I&A UI/Settings.rb
@@ -4,7 +4,7 @@
 module IASummary
 # If you want your Bag Screen to have a scrolling panorama (true or false):
   PANORAMA = true
-# If you want your Bag Screen to have a scrolling panorama (true or false):
-  IAVERSION = 2
+# Which Version the game is? (1 = Amethyst, 2  Iolite):
+  IAVERSION = 1
 
 end
