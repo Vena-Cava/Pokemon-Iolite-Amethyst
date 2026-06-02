3ddddae0e223b4b230e12ceea8da1cc70ebf4ca7
Vena Cava <61953449+Vena-Cava@users.noreply.github.com>
2026-06-02 10:35:01 +0100

    XPX autosave — Pokemon IA XPX

--- Plugins/I&A - Mechanics/Open Screenshots Folder.rb
+++ Plugins/I&A - Mechanics/Open Screenshots Folder.rb
@@ -1,38 +1,33 @@
 #===============================================================================
-# Open Screenshots Folder with F9
+# Open Screenshots Folder with F7
 #===============================================================================
 
 module IAScreenshotFolder
-  KEY = Input::F9
+  KEY = 118   # F7
 
   def self.folder_path
-    save_dir = File.directory?(System.data_directory) ? System.data_directory : "."
-    path = sprintf("%s/Screenshots", save_dir)
+    save_dir = File.expand_path(System.data_directory)
+    path = File.join(save_dir, "Screenshots")
     Dir.mkdir(path) if !Dir.safe?(path)
     return path
   end
 
   def self.open_folder
     path = folder_path.gsub("/", "\\")
-
-    if System.platform[/Windows/i]
-      system("explorer \"#{path}\"")
-    else
-      system("open \"#{folder_path}\"")
-    end
+    system("explorer.exe \"#{path}\"")
   end
 end
 
-class Scene_Map
-  alias ia_screenshot_folder_update update
+module Graphics
+  class << self
+    alias ia_screenshot_folder_update update
 
-  def update
-    ia_screenshot_folder_update
+    def update
+      if Input.triggerex?(IAScreenshotFolder::KEY)
+        IAScreenshotFolder.open_folder
+      end
 
-    echoln "F9 pressed" if Input.triggerex?(VK_F9)
-
-    if Input.triggerex?(VK_F9)
-      IAScreenshotFolder.open_folder
+      ia_screenshot_folder_update
     end
   end
 end
