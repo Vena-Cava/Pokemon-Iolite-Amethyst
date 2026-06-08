f9dd1032c55cee7ea355cd5028e06ebd608b2d50
Vena Cava <61953449+Vena-Cava@users.noreply.github.com>
2026-06-08 14:21:08 +0100

    XPX autosave — Pokemon IA XPX

--- Plugins/[000] Base Essentials/[044] Main/[000] Main.rb
+++
@@ -1,7 +0,0 @@
-#===============================================================================
-# Cuttable Tall Grass: Settings
-#===============================================================================
-module CuttableTallGrass
-# List of Abilities that boost the radius of the Field Move Cut:
-  CUTBOOSTABILITIES = [:HYPERCUTTER, :SHARPNESS]
-end
